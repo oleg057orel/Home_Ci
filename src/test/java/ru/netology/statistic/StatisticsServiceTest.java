875672399c584ec260364f31b5c9431e9a87c9b5
Lazarov_O_M_QAMID_61 <oleg057orel@yandex.ru>
2023-06-06 20:00:54 +0300

    commit JoCoCo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxAtBeginning() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -17,4 +17,17 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxInAverage() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 60, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 60;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
